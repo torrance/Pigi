c5303110eb6692f1c2b43b429b5c38f92c48ea1a
torrance <torrance123@gmail.com>
2022-02-17 08:15:36 +0000

    Test precision flag for read(::MeasurementSet)

--- test/measurementset.jl
+++ test/measurementset.jl
@@ -22,3 +22,18 @@ end
     @test !all(all(isnan.(u.weights)) for u in uvdata)
     @test allunique((u.row, u.chan) for u in uvdata)
 end
+
+@testset "Measurement set precision" begin
+    path = "../../testdata/1215555160/1215555160.ms"
+
+    mset = Pigi.MeasurementSet(path, chanstart=1, chanstop=92)
+
+    uvdata64 = collect(Pigi.read(mset))
+    uvdata32 = collect(Pigi.read(mset, precision=Float32))
+
+    @test typeof(uvdata32[1]) == Pigi.UVDatum{Float32}
+    @test typeof(uvdata64[1]) == Pigi.UVDatum{Float64}
+    @test all(isapprox(u1.u, u2.u) for (u1, u2) in zip(uvdata64, uvdata32))
+    @test all(isapprox(u1.weights, u2.weights) for (u1, u2) in zip(uvdata64, uvdata32))
+    @test all(isapprox(u1.data, u2.data) for (u1, u2) in zip(uvdata64, uvdata32))
+end
