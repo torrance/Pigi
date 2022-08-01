012fed22ae0a06d2919f498566f1efe50039e23e
torrance <torrance123@gmail.com>
2022-08-01 06:03:44 +0000

    Fix coordinate system for tapers; allows us to avoid using resample()

--- src/tapers.jl
+++ src/tapers.jl
@@ -9,31 +9,25 @@ end
 function kaiserbessel(gridspec::GridSpec, ::Type{T}, alpha)::Matrix{T} where {T <: AbstractFloat}
     function kb1D(n, N)
         x = n / N - 0.5
+        @assert -0.5 <= x < 0.5
 
-        if x < -0.5 || x > 0.5
-            return 0.
-        else
-            return besseli(0, π * alpha * sqrt(1 - 4 * x^2)) / besseli(0, π * alpha)
-        end
+        return besseli(0, π * alpha * sqrt(1 - 4 * x^2)) / besseli(0, π * alpha)
     end
 
     return map(CartesianIndices((1:gridspec.Nx,  1:gridspec.Ny))) do xy
         x, y = Tuple(xy)
-        return kb1D(x - 1.5, gridspec.Nx - 1) * kb1D(y - 1.5, gridspec.Ny - 1)
+        return kb1D(x - 1, gridspec.Nx) * kb1D(y - 1, gridspec.Ny)
     end
 end
 
 function blackmanharris(gridspec::GridSpec, ::Type{T})::Matrix{T} where {T <: AbstractFloat}
     function blackmanharris1D(n, N)
-        if n < 0 || n >= N
-            return 0.
-        else
-            return 0.35875 - 0.48829 * cos(2π * (n / N)) + 0.14128 * cos(4π * (n / N)) - 0.01168 * cos(6π * (n / N))
-        end
+        @assert 0 <= n < N
+        return 0.35875 - 0.48829 * cos(2π * (n / N)) + 0.14128 * cos(4π * (n / N)) - 0.01168 * cos(6π * (n / N))
     end
 
     return map(CartesianIndices((1:gridspec.Nx, 1:gridspec.Ny))) do xy
         x, y = Tuple(xy)
-        return blackmanharris1D(x - 1.5, gridspec.Nx - 1) * blackmanharris1D(y - 1.5, gridspec.Ny - 1)
+        return blackmanharris1D(x - 1, gridspec.Nx) * blackmanharris1D(y - 1, gridspec.Ny)
     end
 end
