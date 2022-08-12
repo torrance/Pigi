d476b0c30edbcffa51c8058f2ca359681a4d0fd5
torrance <torrance123@gmail.com>
2022-08-12 08:56:28 +0000

    Remove unnecessary function calls from 2D tapers

--- src/tapers.jl
+++ src/tapers.jl
@@ -14,9 +14,11 @@ function kaiserbessel(gridspec::GridSpec, ::Type{T}, alpha)::Matrix{T} where {T 
         return besseli(0, π * alpha * sqrt(1 - 4 * x^2)) / besseli(0, π * alpha)
     end
 
-    return map(CartesianIndices((1:gridspec.Nx,  1:gridspec.Ny))) do xy
-        x, y = Tuple(xy)
-        return kb1D(x - 1, gridspec.Nx) * kb1D(y - 1, gridspec.Ny)
+    xs = [kb1D(x - 1, gridspec.Nx) for x in 1:gridspec.Nx]
+    ys = [kb1D(y - 1, gridspec.Ny) for y in 1:gridspec.Ny]
+
+    return map(Iterators.product(xs, ys)) do (x, y)
+        return x * y
     end
 end
 
@@ -26,8 +28,10 @@ function blackmanharris(gridspec::GridSpec, ::Type{T})::Matrix{T} where {T <: Ab
         return 0.35875 - 0.48829 * cos(2π * (n / N)) + 0.14128 * cos(4π * (n / N)) - 0.01168 * cos(6π * (n / N))
     end
 
-    return map(CartesianIndices((1:gridspec.Nx, 1:gridspec.Ny))) do xy
-        x, y = Tuple(xy)
-        return blackmanharris1D(x - 1, gridspec.Nx) * blackmanharris1D(y - 1, gridspec.Ny)
+    xs = [blackmanharris1D(x - 1, gridspec.Nx) for x in 1:gridspec.Nx]
+    ys = [blackmanharris1D(y - 1, gridspec.Ny) for y in 1:gridspec.Ny]
+
+    return map(Iterators.product(xs, ys)) do (x, y)
+        return x * y
     end
 end
