bc78c85499573596773ca1fa4aa78f2a426d9e88
Torrance Hodgson <torrance123@gmail.com>
2022-11-08 03:04:21 +0000

    Expand gpugridder test to multiple subgrids of constant w-layer

--- test/gpugridder.jl
+++ test/gpugridder.jl
@@ -1,35 +1,37 @@
-@testset "GPU Gridding" for (precision, atol) in [(Float32, 1e-5), (Float64, 1e-8)]
+@testset "GPU Gridding with multiple subgrids" for (precision, atol) in [(Float32, 1e-5), (Float64, 1e-8)]
+    gridspec = Pigi.GridSpec(1200, 1200, scaleuv=1)
     subgridspec = Pigi.GridSpec(96, 96, scaleuv=1)
 
     subtaper = Pigi.kaiserbessel(subgridspec, precision)
-    Aleft = Aright = ones(SMatrix{2, 2, Complex{precision}, 4}, subgridspec.Nx, subgridspec.Ny)
+    Aterms = ones(SMatrix{2, 2, Complex{precision}, 4}, subgridspec.Nx, subgridspec.Ny)
 
     uvdata = StructVector{Pigi.UVDatum{precision}}(undef, 0)
-    for u in -20:20, v in -20:20
+    for _ in 1:100_000
         push!(uvdata, Pigi.UVDatum{precision}(
-            0, 0, precision(u), precision(v), precision(0), SMatrix{2, 2, precision, 4}(1, 1, 1, 1), rand(SMatrix{2, 2, Complex{precision}, 4})
+            0, 0, 500 * (rand(precision) - 1//2), 500 * (rand(precision) - 1//2), precision(0),
+            SMatrix{2, 2, precision, 4}(1, 1, 1, 1), rand(SMatrix{2, 2, Complex{precision}, 4})
         ))
     end
 
-    workunit = Pigi.WorkUnit{precision}(
-        49, 49, 0, 0, 0, subgridspec, Aleft, Aright, uvdata
-    )
+    workunits = Pigi.partition(uvdata, gridspec, subgridspec, 15, 1, Aterms)
+    @test length(workunits) > 1
+    @test all(w -> w.w0 == 0, workunits)  # These should all be one layer
 
-    cpusubgrid = zeros(Pigi.LinearData{precision}, 96, 96)
-    gpusubgrid = CuArray(cpusubgrid)
+    cpusubgrid = zeros(Pigi.StokesI{precision}, 96, 96)
+    gpusubgrid = GPUArray(cpusubgrid)
 
-    Pigi.gridder!(cpusubgrid, [workunit], subtaper; makepsf=true)
-    Pigi.gridder!(gpusubgrid, [workunit], CuArray(subtaper); makepsf=true)
+    Pigi.gridder!(cpusubgrid, workunits, subtaper; makepsf=true)
+    Pigi.gridder!(gpusubgrid, workunits, GPUArray(subtaper); makepsf=true)
 
     @test all(x -> all(isfinite, x), cpusubgrid)
-    @test all(x -> all(isfinite, x), CuArray(gpusubgrid))
+    @test all(x -> all(isfinite, x), gpusubgrid)
     @test maximum(x -> sum(abs, x[1] - x[2]), zip(Array(gpusubgrid), cpusubgrid)) < atol
 
-    cpusubgrid = zeros(Pigi.LinearData{precision}, 96, 96)
-    gpusubgrid = CuArray(cpusubgrid)
+    cpusubgrid = zeros(Pigi.StokesI{precision}, 96, 96)
+    gpusubgrid = GPUArray(cpusubgrid)
 
-    Pigi.gridder!(cpusubgrid, [workunit], subtaper)
-    Pigi.gridder!(gpusubgrid, [workunit], CuArray(subtaper))
+    Pigi.gridder!(cpusubgrid, workunits, subtaper)
+    Pigi.gridder!(gpusubgrid, workunits, GPUArray(subtaper))
     @test maximum(x -> sum(abs, x[1] - x[2]), zip(Array(gpusubgrid), cpusubgrid)) < atol
 
     # gpusubgrid = [real(x[1]) for x in Array(gpusubgrid)]
