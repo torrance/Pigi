cc582924dd6caacd0ff4022699947c2d14d4a8a6
Torrance Hodgson <torrance@pravic.xyz>
2024-08-20 11:55:47 +0800

    Add raw kernel benchmarking

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -156,6 +156,70 @@ TEMPLATE_TEST_CASE("Predict", "[predict]", float, double) {
     });
 }
 
+TEMPLATE_TEST_CASE("(De)gridder kernels", "[kernels]", float) {
+    if (TESTDATA.empty()) { SKIP("TESTDATA path not provided"); }
+
+    DataTable tbl(TESTDATA, {});
+
+    for (const int kpx : {32, 48, 64, 96, 128}) {
+        GridConfig gridconf {
+            .imgNx = 8000, .imgNy = 8000, .imgScalelm = std::sin(deg2rad(15. / 3600)),
+            .kernelsize = kpx, .kernelpadding = 12
+        };
+
+        auto workunits = partition(tbl, gridconf);
+
+        // Create uvws
+        HostArray<std::array<double, 3>, 1> uvws_h(workunits.size());
+        for (size_t i {}; auto w : workunits) uvws_h[i++] = {w.u, w.v, w.w};
+
+        // Create aterms
+        auto beam_h = Beam::Uniform<double>().gridResponse(
+            gridconf.subgrid(), {0, 0}, 0
+        );
+        DeviceArray<ComplexLinearData<double>, 2> beam_d(beam_h);
+        HostArray<DeviceSpan<ComplexLinearData<double>, 2>, 1> aterms_h(workunits.size());
+        for (auto& aterm : aterms_h) aterm = beam_d;
+
+        // Copy to device
+        DeviceArray<TestType, 2> taper_d {pswf<TestType>(gridconf.subgrid())};
+        DeviceArray<double, 1> lambdas_d(tbl.lambdas());
+        DeviceArray<std::array<double, 3>, 1> uvws_d(uvws_h);
+        DeviceArray<WorkUnit, 1> workunits_d(workunits);
+        DeviceArray<ComplexLinearData<float>, 2> data_d(tbl.data());
+        DeviceArray<LinearData<float>, 2> weights_d(tbl.weights());
+        DeviceArray<DeviceSpan<ComplexLinearData<double>, 2>, 1> alefts_d(aterms_h);
+        DeviceArray<DeviceSpan<ComplexLinearData<double>, 2>, 1> arights_d(aterms_h);
+
+        // Allocate subgrid
+        DeviceArray<StokesI<TestType>, 3> subgrids_d(std::array<long long, 3>{
+            gridconf.subgrid().Nx, gridconf.subgrid().Ny, (long long) workunits.size()
+        });
+
+        HIPCHECK( hipDeviceSynchronize() );
+
+        simple_benchmark(fmt::format(
+            "Gridder kernel={}px nworkunits={}", kpx, workunits.size()
+        ), 1, [&] {
+            gridder<StokesI<TestType>, TestType>(
+                subgrids_d, workunits_d, uvws_d, data_d, weights_d, gridconf.subgrid(),
+                lambdas_d, taper_d, alefts_d, arights_d, 0, false
+            );
+            return true;
+        });
+
+        simple_benchmark(fmt::format(
+            "Degridder kernel={}px nworkunits={}", kpx, workunits.size()
+        ), 1, [&] {
+            degridder<StokesI<TestType>, TestType>(
+                data_d, subgrids_d, workunits_d, uvws_d, lambdas_d, taper_d,
+                alefts_d, arights_d, gridconf.subgrid(), 0, DegridOp::Add
+            );
+            return true;
+        });
+    }
+}
+
 TEST_CASE("Image size", "[imagesize]") {
     if (TESTDATA.empty()) { SKIP("TESTDATA path not provided"); }
 
