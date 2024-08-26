bc40a94fe4e02737b5b625c2fdfbeb6dc721e38a
Torrance Hodgson <torrance@pravic.xyz>
2024-08-26 11:36:57 +0800

    Chunk visibilities for degridder

--- src/degridder.h
+++ src/degridder.h
@@ -15,7 +15,7 @@
 enum class DegridOp {Subtract, Add};
 
 template <typename T, typename S>
-__global__ __launch_bounds__(256)
+__global__ __launch_bounds__(128)
 void _degridder(
     DeviceSpan<ComplexLinearData<float>, 2> data,
     const DeviceSpan<T, 3> subgrids,
@@ -29,13 +29,18 @@ void _degridder(
     const size_t rowoffset,
     const DegridOp degridop
 ) {
+    // To increase the computational intensity of the kernel (just slightly),
+    // we compute $nchunk visibilities at once.
+    const int nchunk {4};
+
     // Set up the shared mem cache
-    const size_t cachesize {256};
+    const size_t cachesize {128};
     __shared__ char _cache[
-        cachesize * (sizeof(ComplexLinearData<S>) + sizeof(std::array<S, 3>))
+        cachesize * (sizeof(ComplexLinearData<S>) + sizeof(std::array<S, 3>) + sizeof(S))
     ];
     auto subgrid_cache = reinterpret_cast<ComplexLinearData<S>*>(_cache);
     auto lmn_cache = reinterpret_cast<std::array<S, 3>*>(subgrid_cache + cachesize);
+    auto thetaoffsets_cache = reinterpret_cast<S*>(lmn_cache + cachesize);
 
     const size_t subgridsize = subgridspec.size();
 
@@ -55,37 +60,42 @@ void _degridder(
             w0 = workunit.w;
         }
 
-        const size_t nchans = chanend - chanstart;
+        // We process $nchunk visibilities at a time, going along a row.
+        // The true channel with of this workunit may not be an even multiple of $nchunk
+        // and thus we may calculcate some edge channels that are not part of this
+        // workunit. These will be discarded later.
+        const size_t nchans = cld<size_t>(chanend - chanstart, nchunk) * nchunk;
         const size_t nvis = (rowend - rowstart) * nchans;
-        const size_t rowstride = lambdas.size();
 
         for (
-            size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
-            idx < blockDim.x * cld<size_t>(nvis, blockDim.x);
-            idx += blockDim.x * gridDim.x
+            size_t idx = blockIdx.x * blockDim.x * nchunk + threadIdx.x * nchunk;
+            idx < blockDim.x * nchunk * cld<size_t>(nvis, blockDim.x * nchunk);
+            idx += blockDim.x * gridDim.x * nchunk
         ) {
-            size_t irow = idx / nchans + rowstart;
-            size_t ichan = idx % nchans + chanstart;
+            const size_t irow = idx / nchans + rowstart;
+            const size_t ichan = idx % nchans + chanstart;
 
-            // Get metadata for this datum
-            S lambda {};
-            std::array<double, 3> uvw;
-            if (idx < nvis) {
-                lambda = lambdas[ichan];
-                uvw = uvws[irow];
+            // Calculate inverse lambda values for each channel in our chunk
+            std::array<S, nchunk> invlambdas;
+            for (int i {}; i < nchunk && ichan + i < chanend; ++i) {
+                invlambdas[i] = 1 / lambdas[ichan + i];
             }
+
+            // Get u,vw for this datum
+            auto [u, v, w] = [&] () -> std::array<S, 3> {
+                auto [u, v, w] = uvws[irow];
+                return {static_cast<S>(u), static_cast<S>(v), static_cast<S>(w)};
+            }();
 
             // We force all data to have positive w values to reduce the number of w layers,
             // since V(u, v, w) = V(-u, -v, -w)^H
             // The data has already been partitioned making this assumption.
-            int signw = std::get<2>(uvw) < 0 ? -1 : 1;
+            int signw = w < 0 ? -1 : 1;
+            u *= -2 * ::pi_v<S> * signw;
+            v *= -2 * ::pi_v<S> * signw;
+            w *= -2 * ::pi_v<S> * signw;
 
-            // Precompute uvw offsets and convert to wavenumbers
-            S u = -2 * ::pi_v<S> * (signw * std::get<0>(uvw) / lambda - u0);
-            S v = -2 * ::pi_v<S> * (signw * std::get<1>(uvw) / lambda - v0);
-            S w = -2 * ::pi_v<S> * (signw * std::get<2>(uvw) / lambda - w0);
-
-            ComplexLinearData<S> datum;
+            std::array<ComplexLinearData<S>, nchunk> datums {};
 
             for (size_t ipx {}; ipx < subgridsize; ipx += cachesize) {
                 const size_t N = min(cachesize, subgridsize - ipx);
@@ -113,45 +123,51 @@ void _degridder(
                     auto n = ndash(l, m);
 
                     lmn_cache[j] = {l, m, n};
+
+                    // Precompute theta offset, which we an do since each block shares
+                    // the same workunit. [dimensionless]
+                    thetaoffsets_cache[j] = -2 * ::pi_v<S> * (u0 * l + v0 * m + w0 * n);
                 }
                 __syncthreads();
 
-                // Zombie threads need to keep filling the cache
-                // but can skip doing any actual work
-                if (idx >= nvis) continue;
-
                 // Cycle through cache
                 for (size_t j {}; j < N; ++j) {
-                    auto [l, m, n] = lmn_cache[j];
-                    auto phase = cis(u * l + v * m + w * n);
-
-                    // Load subgrid cell from the cache
-                    // This shared mem load is broadcast across the warp and so we
+                    // These shared mem loads should be broadcast across the warp and so we
                     // don't need to worry about bank conflicts
-                    auto cell = subgrid_cache[j];
+                    const auto [l, m, n] = lmn_cache[j];
+                    const auto cell = subgrid_cache[j];
 
-                    // Equivalent of: data += cell * phase
-                    // Written out explicitly to use fma operations
-                    cmac(datum.xx, cell.xx, phase);
-                    cmac(datum.yx, cell.yx, phase);
-                    cmac(datum.xy, cell.xy, phase);
-                    cmac(datum.yy, cell.yy, phase);
+                    const S theta {u * l + v * m + w * n};  // [meters]
+                    const S thetaoffset {thetaoffsets_cache[j]};  // [dimensionless]
+
+                    for (int k {}; k < nchunk; ++k) {
+                        const auto phase = cis(theta * invlambdas[k] - thetaoffset);
+
+                        // Equivalent of: data += cell * phase
+                        // Written out explicitly to use fma operations
+                        cmac(datums[k].xx, cell.xx, phase);
+                        cmac(datums[k].yx, cell.yx, phase);
+                        cmac(datums[k].xy, cell.xy, phase);
+                        cmac(datums[k].yy, cell.yy, phase);
+                    }
                 }
             }
 
-            // If w was negative, we need to take the adjoint before storing the datum value
-            if (signw == -1) datum = datum.adjoint();
+            if (irow < rowend) {
+                for (int i {}; i < nchunk && ichan + i < chanend; ++i) {
+                    // If w was negative, we need to take the adjoint before storing the datum value
+                    if (signw == -1) datums[i] = datums[i].adjoint();
 
-            if (idx < nvis) {
-                switch (degridop) {
-                case DegridOp::Add:
-                    data[irow * rowstride + ichan]
-                        += static_cast<ComplexLinearData<float>>(datum);
-                    break;
-                case DegridOp::Subtract:
-                    data[irow * rowstride + ichan]
-                        -= static_cast<ComplexLinearData<float>>(datum);
-                    break;
+                    switch (degridop) {
+                    case DegridOp::Add:
+                        data[irow * lambdas.size() + ichan + i]
+                            += static_cast<ComplexLinearData<float>>(datums[i]);
+                        break;
+                    case DegridOp::Subtract:
+                        data[irow * lambdas.size() + ichan + i]
+                            -= static_cast<ComplexLinearData<float>>(datums[i]);
+                        break;
+                    }
                 }
             }
         }  // idx loop
@@ -177,7 +193,7 @@ void degridder(
     auto fn = _degridder<T, S>;
 
     // x-dimension distributes uvdata
-    uint32_t nthreadsx {256};
+    uint32_t nthreadsx {128};
     uint32_t nblocksx {1};
 
     // y-dimension breaks the subgrid down into 8 blocks
