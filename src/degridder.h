ee1b2f287f09059c58edabe6541f8284f74bfbdd
Torrance Hodgson <torrance@pravic.xyz>
2024-10-03 12:28:31 +0800

    Add row-optimization to degridder kernel

--- src/degridder.h
+++ src/degridder.h
@@ -14,7 +14,7 @@
 
 enum class DegridOp {Subtract, Add};
 
-template <typename T, typename S, uint32_t cachesize, uint32_t nchunk>
+template <typename T, typename S, uint32_t cachesize, uint32_t warpsize, uint32_t nchunk>
 __global__ __launch_bounds__(128)
 void _degridder(
     DeviceSpan<ComplexLinearData<float>, 2> data,
@@ -37,9 +37,9 @@ void _degridder(
     const uint32_t rowstride = lambdas.size();
 
     // Calculate warp properties
-    const uint32_t warpid = threadIdx.x / warpSize;
-    const uint32_t warprank = threadIdx.x % warpSize;
-    const uint32_t warpranknext = (warprank + 1) % warpSize;
+    const uint32_t warpid = threadIdx.x / warpsize;
+    const uint32_t warprank = threadIdx.x % warpsize;
+    const uint32_t warpranknext = (warprank + 1) % warpsize;
 
     // y block index denotes workunit
     for (uint32_t wid {blockIdx.y}; wid < workunits.size(); wid += gridDim.y) {
@@ -58,8 +58,6 @@ void _degridder(
         }
 
         uint32_t nchans = chanend - chanstart;
-        uint32_t nrows = rowend - rowstart;
-        uint32_t nvis = nrows * nchans;
 
         for (
             uint32_t idx = blockIdx.x * blockDim.x * nchunk + threadIdx.x * nchunk;
@@ -91,90 +89,93 @@ void _degridder(
                 thetaoffsets[i] = -2 * ::pi_v<S> * (u0 * l + v0 * m + w0 * n);
             }
 
-            // Cycle over $warpSize visibilities until exhausted (rounded up to multiple
-            // of $warpSize)
-            const uint32_t N = cld<uint32_t>(nvis, warpSize) * warpSize;
-            for (uint32_t i {warprank}; i < N; i += warpSize) {
-                uint32_t irow = rowstart + i / nchans;
-                uint32_t ichan = chanstart + i % nchans;
+            const uint32_t N = cld<uint32_t>(nchans, warpsize) * warpsize;
 
-                short wsign {};
-                S u {}, v {}, w {};
-                ComplexLinearData<S> datum {};
-                if (i < nvis) {
-                    auto [u_, v_, w_] = uvws[irow];
-                    u = u_; v = v_; w = w_;
+            for (uint32_t irow {rowstart}; irow < rowend; ++irow) {
+                auto uvw = uvws[irow];
+                S u = std::get<0>(uvw), v = std::get<1>(uvw), w = std::get<2>(uvw);
 
-                    // We force all data to have positive w values to reduce the number of w layers,
-                    // since V(u, v, w) = V(-u, -v, -w)^H
-                    // The data has already been partitioned making this assumption.
-                    wsign = w < 0 ? -1 : 1;
+                // We force all data to have positive w values to reduce the number of w layers,
+                // since V(u, v, w) = V(-u, -v, -w)^H
+                // The data has already been partitioned making this assumption.
+                short wsign = w < 0 ? -1 : 1;
 
-                    S lambda = lambdas[ichan];
-                    u *= -2 * ::pi_v<S> * wsign / lambda;
-                    v *= -2 * ::pi_v<S> * wsign / lambda;
-                    w *= -2 * ::pi_v<S> * wsign / lambda;
+                // Precompute theta in _meters_ By doing this here, we can compute the true
+                // theta by a multiplication (by inverse lambda) in the host path.
+                std::array<S, nchunk> thetas;
+                for (uint32_t i {}; i < nchunk; ++i) {
+                    auto [l, m, n] = lmns[i];
+                    thetas[i] = -2 * ::pi_v<S> * (u * l + v * m + w * n) * wsign;  // [meters]
                 }
 
-                // Reduce the data by daisy-chaining the visbilities around
-                // the warp group, each reducing its own set of pixels onto the datum
-                for (uint32_t j {}; j < warpSize; ++j) {
-                    for (uint32_t k {}; k < nchunk; ++k) {
-                        auto [l, m, n] = lmns[k];
-                        auto phase = cis(u * l + v * m + w * n - thetaoffsets[k]);
+                // Cycle over $warpsize visibilities from the channel until exhausted
+                // (rounded up to an even multiple of $warpsize)
+                for (uint32_t i {warprank}; i < N; i += warpsize) {
+                    uint32_t ichan = chanstart + i;
 
-                        // Equivalent of: data += cell * phase
-                        // Written out explicitly to use fma operations
-                        cmac(datum.xx, cells[k].xx, phase);
-                        cmac(datum.yx, cells[k].yx, phase);
-                        cmac(datum.xy, cells[k].xy, phase);
-                        cmac(datum.yy, cells[k].yy, phase);
+                    // Create and populate registry cache
+                    S invlambda {};
+                    ComplexLinearData<S> datum {};
+                    if (ichan < chanend) {
+                        invlambda = 1 / static_cast<S>(lambdas[ichan]);
                     }
 
-                    // Cyclically shuffle the visibility data around the warp
-                    assert(__activemask() == 0xffffffff);
-                    wsign = __shfl(wsign, warpranknext);
-                    u = __shfl(u, warpranknext);
-                    v = __shfl(v, warpranknext);
-                    w = __shfl(w, warpranknext);
+                    // Reduce the data by daisy-chaining the visbilities around
+                    // the warp group, each reducing its own set of pixels onto the datum
+                    for (uint32_t j {}; j < warpsize; ++j) {
+                        for (uint32_t k {}; k < nchunk; ++k) {
+                            auto phase = cis(thetas[k] * invlambda - thetaoffsets[k]);
 
-                    auto ptr = reinterpret_cast<S*>(&datum);
-                    for (uint32_t k {}; k < 8; ++k) {
-                        ptr[k] = __shfl(ptr[k], warpranknext);
+                            // Equivalent of: data += cell * phase
+                            // Written out explicitly to use fma operations
+                            cmac(datum.xx, cells[k].xx, phase);
+                            cmac(datum.yx, cells[k].yx, phase);
+                            cmac(datum.xy, cells[k].xy, phase);
+                            cmac(datum.yy, cells[k].yy, phase);
+                        }
+
+                        // Cyclically shuffle the visibility data around the warp
+                        assert(__activemask() == 0xffffffff);
+                        invlambda = __shfl(invlambda, warpranknext, warpsize);
+
+                        auto ptr = reinterpret_cast<S*>(&datum);
+                        for (uint32_t k {}; k < 8; ++k) {
+                            ptr[k] = __shfl(ptr[k], warpranknext, warpsize);
+                        }
                     }
-                }
 
-                // If we forced w positive, it's time to take the conjugate transpose
-                if (wsign == -1) datum = datum.adjoint();
+                    // If we forced w positive, it's time to take the conjugate transpose
+                    if (wsign == -1) datum = datum.adjoint();
 
-                // Write results out to shared memory...
-                __syncthreads();
-                data_cache[threadIdx.x] = datum;
+                    // Write results out to shared memory...
+                    __syncthreads();
+                    data_cache[threadIdx.x] = datum;
 
-                // ...and then reduce across warps
-                __syncthreads();
-                for (uint32_t j {warpSize + threadIdx.x}; j < blockDim.x; j += warpSize) {
-                    datum += data_cache[j];
-                }
-
-                // Warp 0 is responsible for finally writing to global memory
-                if (i < nvis && warpid == 0) {
-                    switch (degridop) {
-                    case DegridOp::Add:
-                        atomicAdd(
-                            data.data() + irow * rowstride + ichan,
-                            static_cast<ComplexLinearData<float>>(datum)
-                        );
-                        break;
-                    case DegridOp::Subtract:
-                        atomicSub(
-                            data.data() + irow * rowstride + ichan,
-                            static_cast<ComplexLinearData<float>>(datum)
-                        );
-                        break;
+                    // ...and then reduce across warps
+                    __syncthreads();
+                    for (uint32_t j {warpsize + threadIdx.x}; j < blockDim.x; j += warpsize) {
+                        datum += data_cache[j];
                     }
-                }
-            }
+
+                    // Warp 0 is responsible for finally writing to global memory
+                    if (warpid == 0 && ichan < chanend) {
+                        switch (degridop) {
+                        case DegridOp::Add:
+                            atomicAdd(
+                                data.data() + irow * rowstride + ichan,
+                                static_cast<ComplexLinearData<float>>(datum)
+                            );
+                            break;
+                        case DegridOp::Subtract:
+                            atomicSub(
+                                data.data() + irow * rowstride + ichan,
+                                static_cast<ComplexLinearData<float>>(datum)
+                            );
+                            break;
+                        }
+                    }
+                }  // chan loop
+            }  // row loop
         }  // idx loop
     }  // workunit loop
 }
@@ -195,7 +196,7 @@ void degridder(
 ) {
     auto timer = Timer::get("predict::batch::degridder");
 
-    auto fn = _degridder<T, S, 128, 8>;
+    auto fn = _degridder<T, S, 128, 16, 8>;
 
     // x-dimension distributes uvdata
     uint32_t nthreadsx {128};
